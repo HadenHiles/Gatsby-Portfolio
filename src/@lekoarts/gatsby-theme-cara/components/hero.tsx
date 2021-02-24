8d2cbb62726db78cf12d98d8b795faeeead41371
HadenHiles <haden@howtohockey.com>
2021-02-24 11:42:31 -0500

    Remove theme name from footer

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -38,7 +38,6 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
       <SVG icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
       <SVG icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
       <SVG icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
-      <img  src={withPrefix(`/portrait.svg`)} alt="Haden" />
     </Divider>
     <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={offset} factor={factor}>
       <Inner>
